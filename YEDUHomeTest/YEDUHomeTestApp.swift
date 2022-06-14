3f567c135e8d9fa650b85b2a2532c36414d6259a
hoangnart5 <hoangtv@coccoc.com>
2022-06-14 22:52:26 +0700

    Initial Commit

---
+++ YEDUHomeTest/YEDUHomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  YEDUHomeTestApp.swift
+//  YEDUHomeTest
+//
+//  Created by MACBOOK on 14/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct YEDUHomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
